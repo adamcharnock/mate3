667f7d95f5406e689361f3ebff0b3878a8192947
Adam Charnock <adam@adamcharnock.com>
2019-09-08 00:26:32 +0100

    Removing no longer used code

--- mate3/io.py
+++ mate3/io.py
@@ -52,61 +52,6 @@ def int_to_ip_address(int_ip_address: int):
     return socket.inet_ntoa(struct.pack("!I", int_ip_address))
 
 
-def get_common_block(client: ModbusClient, basereg):
-    """ Read and return the sunspec common information
-    block.
-
-    :returns: A dictionary of the common block information
-    """
-    length = 69
-    response = client.read_holding_registers(basereg, length + 2)
-    decoder = BinaryPayloadDecoder.fromRegisters(
-        response.registers, byteorder=Endian.Big, wordorder=Endian.Big
-    )
-
-    return {
-        "SunSpec_ID": decoder.decode_32bit_uint(),
-        "SunSpec_DID": decoder.decode_16bit_uint(),
-        "SunSpec_Length": decoder.decode_16bit_uint(),
-        "Manufacturer": decoder.decode_string(size=32),
-        "Model": decoder.decode_string(size=32),
-        "Options": decoder.decode_string(size=16),
-        "Version": decoder.decode_string(size=16),
-        "SerialNumber": decoder.decode_string(size=32),
-        "DeviceAddress": decoder.decode_16bit_uint(),
-        "Next_DID": decoder.decode_16bit_uint(),
-        "Next_DID_Length": decoder.decode_16bit_uint(),
-    }
-
-
-def read_sun_spec_header(client: ModbusClient, basereg):
-    # Read two bytes from basereg, a SUNSPEC device will start with 0x53756e53
-    # As 8bit ints they are 21365, 28243
-
-    response = client.read_holding_registers(basereg, 2)
-    if response.registers[0] == 21365 and response.registers[1] == 28243:
-        logging.info(".. SunSpec device found. Reading Manufacturer info")
-    else:
-        return None
-
-    # There is a 16 bit string at basereg + 4 that contains Manufacturer
-    response = client.read_holding_registers(basereg + 4, 16)
-
-    decoder = BinaryPayloadDecoder.fromRegisters(
-        response.registers, byteorder=Endian.Big, wordorder=Endian.Big
-    )
-    manufacturer = decoder.decode_string(16)
-    if b"OUTBACK_POWER" in manufacturer.upper():
-        logging.info(".. Outback Power device found")
-    else:
-        logging.info(".. Not an Outback Power device. Detected " + manufacturer)
-        return None
-
-    register = client.read_holding_registers(basereg + 3)
-    block_size = int(register.registers[0])
-    return block_size
-
-
 def read_block(client: ModbusClient, basereg) -> Tuple[Optional[int], Optional[Device]]:
     register = client.read_holding_registers(basereg)
     device_id = register.registers[0]
